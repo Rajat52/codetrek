95ad68340d200bb5b589d58db3bd4fcc6360e16e
Rajat52 <rajatbatoe52@gmail.com>
2018-10-01 01:03:01 +0530

    task 5 completed of day 3

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-function myfunction()
-{
-   if (document.forms["frm"].answer.value == ""); 
-{alert("empty");
+function myfunction(){
+	alert("empty");
+	if (document.forms["frm"].answer.value == ""){
+		alert("empty");
+	}
+	if (document.forms["frm"].description.value == ""){
+		alert("empty description");
+	}
+}   
+function like(){
+	var like_button = document.getElementById('like');
+	like_button.classList.add("text-success");
 }
-if (document.forms["frm"].description.value == ""); 
-{alert("empty description");
+function dislike(){
+	var dislike_button = document.getElementById('dislike');
+	dislike_button.classList.add("text-danger");
 }
-     
-  }       
