0a38ebdb08e32ff4dea82d47e0982fcb9eb553ba
Rajat52 <rajatbatoe52@gmail.com>
2018-10-02 16:10:22 +0530

    add new files

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ function myfunction(){
 	if (document.forms["frm"].description.value == ""){
 		alert("empty description");
 	}
-}   
+} 
 function like(){
 	var like_button = document.getElementById('like');
 	like_button.classList.add("text-success");
-}
+	
+    }
 function dislike(){
 	var dislike_button = document.getElementById('dislike');
 	dislike_button.classList.add("text-danger");
+
 }
